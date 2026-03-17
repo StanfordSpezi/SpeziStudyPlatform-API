2258e5f819a5711fb04bc5ee1f4041350ed54c36
Philipp Nagy <philipp.nagy@tum.de>
2026-03-17 15:14:54 -0700

    StudyDetailContent title not optional

--- Sources/SpeziStudyPlatformAPITypes/DomainTypes/StudyDetailContent.swift
+++ Sources/SpeziStudyPlatformAPITypes/DomainTypes/StudyDetailContent.swift
@@ -9,12 +9,12 @@
 
 /// Note: This type is mapped from Components.Schemas.StudyDetailContent via typeOverrides in openapi-generator-config.yaml
 public struct StudyDetailContent: Codable, Sendable, Hashable {
-    public var title: String?
+    public var title: String
     public var shortTitle: String?
     public var explanationText: String?
     public var shortExplanationText: String?
 
-    public init(title: String? = nil, shortTitle: String? = nil, explanationText: String? = nil, shortExplanationText: String? = nil) {
+    public init(title: String, shortTitle: String? = nil, explanationText: String? = nil, shortExplanationText: String? = nil) {
         self.title = title
         self.shortTitle = shortTitle
         self.explanationText = explanationText
